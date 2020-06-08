5a6202a363348b642383baf10719262801e81a79
Gautam J <gautam.jayapal@gmail.com>
2020-06-08 17:32:43 +0530

    added delete all feedbacks functionality

--- Review/urls.py
+++ Review/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import ListFeedbackView, SearchResultsView
+from .views import ListFeedbackView, SearchResultsView, deleteAllFeedback
 
 urlpatterns = [
     path('', ListFeedbackView.as_view(), name='ListFeedbackView'),
     path('search/', SearchResultsView.as_view(), name='SearchResultsView'),
+    path('delete/', deleteAllFeedback, name='deleteAllFeedback'),
 ]
--- Review/views.py
+++ Review/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import render
 from django.views.generic import ListView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from Feedback.models import FeedbackData
@@ -21,4 +22,12 @@ class SearchResultsView(ListView):
     def get_queryset(self):
         query = self.request.GET.get('q')
         object_list = FeedbackData.objects.filter(teacher_name__name__icontains=query)
+        object_list = object_list.order_by('-date_submitted')
         return object_list
+
+
+def deleteAllFeedback(request):
+    FeedbackData.objects.all().delete()
+    context = {}
+
+    return render(request, 'Review/delete_feedback.html', context)
