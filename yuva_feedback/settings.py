56bfa6ed86af52e2019a4ce9424a4057209cdc49
Gautam J <gautam.jayapal@gmail.com>
2020-06-08 10:53:52 +0530

    running website

--- yuva_feedback/settings.py
+++ yuva_feedback/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'k-o1^hequ5ed*p^z*4x@-^^s8t^gx%qj37q+rxn789b4fmj(9v'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -105,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Kolkata'
 
 USE_I18N = True
 
