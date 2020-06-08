6af8e4175e0e286161e68d30f4354da13d2c20dd
Gautam J <gautam.jayapal@gmail.com>
2020-06-08 15:50:43 +0530

    changed FeedbackView to ListView

--- Review/views.py
+++ Review/views.py
@@ -1,6 +1,11 @@
-from django.views.generic import TemplateView
+from django.views.generic import ListView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from Feedback.models import FeedbackData
 
 
-class ListFeedbackView(LoginRequiredMixin, TemplateView):
+class ListFeedbackView(LoginRequiredMixin, ListView):
+    model = FeedbackData
     template_name = 'Review/list_feedback.html'
+    context_object_name = 'feedbacks'
+    ordering = ['-date_submitted']
+    paginate_by = 10
