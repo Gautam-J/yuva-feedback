8f1f99e70f7db1f5c4e71480f317584bdee56fd8
Gautam J <gautam.jayapal@gmail.com>
2020-06-08 16:43:30 +0530

    added search for feedback listview

--- Review/urls.py
+++ Review/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import ListFeedbackView
+from .views import ListFeedbackView, SearchResultsView
 
 urlpatterns = [
     path('', ListFeedbackView.as_view(), name='ListFeedbackView'),
+    path('search', SearchResultsView.as_view(), name='SearchResultsView')
 ]
--- Review/views.py
+++ Review/views.py
@@ -9,3 +9,16 @@ class ListFeedbackView(LoginRequiredMixin, ListView):
     context_object_name = 'feedbacks'
     ordering = ['-date_submitted']
     paginate_by = 10
+
+
+class SearchResultsView(ListView):
+    model = FeedbackData
+    template_name = 'Review/list_feedback.html'
+    context_object_name = 'feedbacks'
+    ordering = ['-date_submitted']
+    paginate_by = 10
+
+    def get_queryset(self):
+        query = self.request.GET.get('q')
+        object_list = FeedbackData.objects.filter(teacher_name__name__icontains=query)
+        return object_list
