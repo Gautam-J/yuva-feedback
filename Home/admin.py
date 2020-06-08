9b7b07e9d23cf75618df25555990a6ebef2a760c
Gautam J <gautam.jayapal@gmail.com>
2020-06-08 11:10:05 +0530

    added Teacher model

--- Home/admin.py
+++ Home/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Teacher
 
-# Register your models here.
+
+class TeacherAdmin(admin.ModelAdmin):
+    search_fields = ['name']
+
+
+admin.site.register(Teacher, TeacherAdmin)
--- Home/models.py
+++ Home/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Teacher(models.Model):
+    name = models.CharField(verbose_name='Name of teacher',
+                            max_length=255)
+
+    def __str__(self):
+        return self.name
