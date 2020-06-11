98a68424789b8a61445be1d72490c3787c9d0224
Gautam J <gautam.jayapal@gmail.com>
2020-06-11 12:42:30 +0530

    added static .css files

--- yuva_feedback/settings.py
+++ yuva_feedback/settings.py
@@ -122,5 +122,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    'base_templates',
+]
+
 LOGIN_URL = 'LoginView'
 LOGIN_REDIRECT_URL = 'ListFeedbackView'
